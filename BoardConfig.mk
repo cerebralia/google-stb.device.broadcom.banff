7234b497fae4e42055e9b870b1bd8ba900b33f1b
Pierre Couillaud <pierre@broadcom.com>
2015-12-02 15:48:21 -0800

    arrow: framework for clang build integration.
    refs #SWANDROID-2756
    
    Change-Id: I3a9bbe606e4b884c4685ffe593f2b8e92ba9dcde
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,5 +164,8 @@ NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
 TARGET_BOARD_KERNEL_HEADERS := device/google/arrow/kernel-headers
 
+# set to 'true' for clang integration.
+USE_CLANG_PLATFORM_BUILD := false
+
 include device/google/arrow/bcm_refsw.mk
 
