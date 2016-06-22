ceb4c98c52f4c97763c4ba001814e76ae551cc1c
Pierre Couillaud <pierre.couillaud@broadcom.com>
2016-06-22 06:11:58 -0700

    nexus: adding initial export files for .ko reduction.
    refs #SW7435-937
    
    Change-Id: I6ab3792956ea477bf769fc905544d4ab31b70939
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- settings.mk
+++ settings.mk
@@ -77,3 +77,5 @@ export BOLT_IMG_TO_USE_OVERRIDE :=
 export ANDROID_USES_BORINGSSL := y
 export NEXUS_HDMI_INPUT_SUPPORT := y
 export NEXUS_C_STD := c99
+export NEXUS_EXPORT_FILE := ${ANDROID}/${BCM_VENDOR_STB_ROOT}/bcm_platform/nxif/nexus_export_file.txt
+
