696504f72fbbd7ca3bd872ce59cefaf1d95f08b0
Pierre Couillaud <pierre@broadcom.com>
2016-06-22 21:17:38 -0700

    banff: sync up avko-n into banff for android 7.0 (II)
    refs #SWANDROID-2756
    
    Change-Id: I85d1ca06d87e7c9f4ff444b515a92480b9f06155
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- settings.mk
+++ settings.mk
@@ -80,3 +80,6 @@ export NEXUS_HDMI_INPUT_SUPPORT := y
 export NEXUS_C_STD := c99
 export NEXUS_EXPORT_FILE := ${ANDROID}/${BCM_VENDOR_STB_ROOT}/bcm_platform/nxif/nexus_export_file.txt
 
+export ANDROID_SUPPORTS_WIDEVINE := n
+export ANDROID_SUPPORTS_PLAYREADY := n
+
