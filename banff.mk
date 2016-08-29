be440d2d2e16661a837404c3a3ea59a2870a234f
Patrick Pun <patrick.pun@broadcom.com>
2016-08-28 22:56:37 -0700

    Banff: Enable warm/cold boot splash screen
    refs #SWANDROID-3410
    
    Change-Id: I29a2601e3d279f98ca41126706a58b28015d3a58

--- banff.mk
+++ banff.mk
@@ -200,6 +200,7 @@ ifeq (,$(filter redux,$(LOCAL_RUN_TARGET)))
       BcmTVInput \
       BcmOtaUpdater \
       BcmKeyInterceptor \
+      BcmSplash \
       camera.banff \
       gralloc.banff \
       hdmi_cec.banff \
