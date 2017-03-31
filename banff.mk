9cc69c8b2254188c8d6f2753cb917649f120f7e0
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-03-31 11:54:26 -0700

    gpio_keys_polled: rename module to match device name.
    refs #SWANDROID-3918
    
    Change-Id: Iaeb89afe0eeb8b3896cefa45eeff89a206986529
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- banff.mk
+++ banff.mk
@@ -37,7 +37,7 @@ export LOCAL_DEVICE_MEDIA
 export LOCAL_DEVICE_OVERLAY      := device/broadcom/banff/overlay
 export LOCAL_DEVICE_SEPOLICY_BLOCK := device/broadcom/banff/sepolicy-block
 export LOCAL_DEVICE_AON_GPIO     := device/broadcom/banff/aon_gpio.cfg:system/vendor/power/aon_gpio.cfg
-export LOCAL_DEVICE_KEY_POLL     := device/broadcom/common/keylayout/gpio_keys_polled.kl:system/usr/keylayout/gpio_keys_polled_5.kl
+export LOCAL_DEVICE_KEY_POLL     := device/broadcom/common/keylayout/gpio_keys_polled.kl:system/usr/keylayout/gpio_keys_polled.kl
 export LOCAL_DEVICE_REFERENCE_BUILD := device/broadcom/banff/reference_build.mk
 export LOCAL_DEVICE_BT_CONFIG    := device/broadcom/banff/bluetooth/vnd_banff.txt
 export LOCAL_DEVICE_USERDATA     := 6137298432 # ~5.7G
